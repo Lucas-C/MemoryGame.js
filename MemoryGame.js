3ad89700e78e118f011f76475b78b653d839e1c6
Lucas Cimon <lucas.cimon@gmail.com>
2016-06-12 09:49:46 +0100

    Adding arrival transition effect + panicMode

--- MemoryGame.js
+++ MemoryGame.js
@@ -1,165 +1,102 @@
+var PULSE_DURATION = 1000;
+
 var MemoryGame = function () {
-    "use strict";
-        var lvls = {
-            'easy': {'rows': 3, 'cols': 4, 'matches': 2, 'simultaneouslyRevealed': 1},
-            'hard': {'rows': 5, 'cols': 6, 'matches': 5, 'simultaneouslyRevealed': 2}
-        },
-        start = function (levelName) {
-            var lvl = lvls[levelName];
-            var currentLvl = new Level(lvl.rows, lvl.cols, lvl.matches, lvl.simultaneouslyRevealed);
-            var info = document.getElementById('game-info');
-            currentLvl.onwin = function (clicks, prc) {
-                info.innerHTML = 'You\'ve found all matches in <strong>' + clicks + '</strong> clicks with <strong>' + prc + '%</strong> efficiency';
-            };
-            info.innerHTML = 'Click the cards to reveal <strong>' + lvl.matches + '</strong> matches';
-        };
+    'use strict';
+    var lvls = {
+        'easy':   {'rows': 3, 'cols': 4, 'requiredMatches': 2, 'simultaneouslyRevealed': 1},
+        'medium': {'rows': 4, 'cols': 7, 'requiredMatches': 2, 'simultaneouslyRevealed': 1, 'panicMode': true},
+        'hard':   {'rows': 5, 'cols': 6, 'requiredMatches': 5, 'simultaneouslyRevealed': 2},
+    }
 
     this.changeLevel = function (btn, levelName) {
+        // We update the buttons states:
         [].forEach.call(document.getElementsByClassName('lvlButton'), function(btn) {
             btn.disabled = false;
         });
-        start(btn.textContent);
         btn.disabled = true;
+
+        // We remove the old Level:
+        var playfieldWrapper = document.getElementById('playfield-wrapper');
+        playfieldWrapper.childNodes[0] = document.createTextNode('&nbsp;');
+        playfieldWrapper.className = '';
+
+        // We initialize the new Level:
+        var lvl = lvls[btn.textContent];
+        var currentLvl = new Level(lvl);
+
+        // We set the help message
+        var infoTextNode = document.getElementById('game-info');
+        infoTextNode.innerHTML = 'Click the cards to reveal <strong>' + lvl.requiredMatches + '</strong> matches';
+
+        // We set the victory message
+        currentLvl.onwin = function (clicks, prc) {
+            infoTextNode.innerHTML = 'You\'ve found all matches in <strong>' + clicks + '</strong> clicks with <strong>' + prc + '%</strong> efficiency';
+        };
     }
     
     var firstLvlButton = document.getElementsByClassName('lvlButton')[0];
     this.changeLevel(firstLvlButton);
 };
 
-var Level = function (rows, cols, requiredMatchesCount, simultaneouslyRevealed) {
+var Level = function (lvlParams) {
     "use strict";
     
-    if (simultaneouslyRevealed >= requiredMatchesCount) {
-        throw new Error('Incorrect level paremeters');
-    }
-
-    var cardsList           = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW[\\]^_`abcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ',
-        playfieldWrapper    = document.getElementById('playfield-wrapper'),
-        playfield           = document.createElement('table'),
-        cards               = [],
-        self                = this,
-        clicksCnt           = 0,
-        matchCount          = 0,
-        openCards           = [],
-        Card                = function (text, pair) {
-            this.state      = 'faceDown';
-            this.freezed    = 0;
-            this.text       = text;
-            this.pair       = pair;
-            this.clicksCnt     = 0;
-
-            var flipper = null,
-                front   = null,
-                back    = null,
-                clicks  = null;
-
-            this.draw = function (idx, container) {
-                var card    = document.createElement('div'),
-                    txt     = document.createTextNode(this.text);
-
-                flipper = card.cloneNode(false);
-
-                front   = card.cloneNode(false);
-                back    = card.cloneNode(false);
-                clicks  = card.cloneNode(false);
-
-                card.className = 'card';
-                flipper.className = 'flipper';
-                front.className = 'front face icon';
-                back.className = 'back face';
-                clicks.className = 'clicks';
-                clicks.appendChild(document.createTextNode('\xA0'));
-
-                front.appendChild(txt);
-                front.appendChild(clicks);
-
-                txt = txt.cloneNode(false);
-                txt.nodeValue = '\xA0';
-
-                back.appendChild(txt);
-
-                flipper.appendChild(back);
-                flipper.appendChild(front);
-
-                flipper.setAttribute('idx', idx);
-
-                card.appendChild(flipper);
-                container.appendChild(card);
-            };
-
-            this.flip = function (state) {
-                if (state === 'faceUp') {
-                    flipper.className = 'flipper flipfront';
-                    front.style.opacity = 1;
-                    back.style.opacity = 0;
-
-                    this.state = 'faceUp';
-                    this.clicksCnt++;
-
-                    clicks.childNodes[0].nodeValue = this.clicksCnt;
-
-                    clicksCnt++;
-                } else if (state === 'faceDown') {
-                    flipper.className = 'flipper flipback';
-                    front.style.opacity = 0;
-                    back.style.opacity = 1;
-                    this.state = 'faceDown';
-                }
-            };
-
-            this.pulse = function () {
-                flipper.className = 'flipper flipfront pulse';
-                window.setTimeout(function () {
-                    flipper.parentNode.style.opacity = '0.3';
-                }, 1000);
-            };
-        },
+    var cardsList            = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVW[\\]^_`abcdefghijklmnopqrstuvwxyzÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ'.split(''),
+        playfieldWrapper     = document.getElementById('playfield-wrapper'),
+        playfield            = document.createElement('div'),
+        cards                = [],
+        self                 = this,
+        matchCount           = 0,
+        openCards            = [],
+        lastPanicTranslation = 'vertical',
         prepare = function () {
-            for (var i = 0; i < (rows * cols) / requiredMatchesCount; i++) {
-                for (var j = 0; j < requiredMatchesCount; j++) {
-                    cards.push(new Card(cardsList[i], i));
+            for (var i = 0; i < (lvlParams.rows * lvlParams.cols) / lvlParams.requiredMatches; i++) {
+                for (var j = 0; j < lvlParams.requiredMatches; j++) {
+                    cards.push(new Card(cardsList[i], i, self));
                 }
             }
             cards.shuffle();
         },
         draw = function () {
-            var tbody       = document.createElement('tbody'),
-                row         = document.createElement('tr'),
-                cell        = document.createElement('td'),
-                rowFrag     = document.createDocumentFragment(),
-                cellFrag    = document.createDocumentFragment(),
-                k           = 0;
+            var playfieldFrag = document.createDocumentFragment();
 
-            for (var i = 0; i < rows; i = i + 1) {
-                row = row.cloneNode(false);
-
-                for (var j = 0; j < cols; j = j + 1) {
-                    cell = cell.cloneNode(false);
-
-                    cards[k].draw(k, cell);
-                    cellFrag.appendChild(cell);
-
-                    k++;
+            for (var i = 0; i < lvlParams.rows; i++) {
+                for (var j = 0; j < lvlParams.cols; j++) {
+                    var cardIndex = i * lvlParams.cols + j,
+                        card = cards[cardIndex];
+                    card.draw(cardIndex);
+                    card.domElement.setAttribute('idx', cardIndex);
+                    card.domElement.style.top = (i * 90) + 'px';
+                    card.domElement.style.left = '-1000px'; // Initially positionned outside the screen, on the left
+                    playfieldFrag.appendChild(card.domElement);
+                    // Triggering the arrival transitions, waterfall-style
+                    setTimeout(function (card, j) {
+                        card.domElement.style.left = (j * 90) + 'px';
+                    }, 100 * cardIndex, card, j);
                 }
-
-                row.appendChild(cellFrag);
-                rowFrag.appendChild(row);
             }
 
-            tbody.appendChild(rowFrag);
-            playfield.appendChild(tbody);
-            playfieldWrapper.replaceChild(playfield, playfieldWrapper.childNodes[0]);
+            playfield.appendChild(playfieldFrag);
+            playfieldWrapper.replaceChild(playfield, playfieldWrapper.childNodes[0]); // replace TextNode('&nbsp;')
         },
         play = function (event) {
             var srcElement = event.srcElement;
             if (srcElement.classList.contains('face')) {
                 srcElement = srcElement.parentNode;
             }
-            if (!srcElement.classList.contains('flipper')) {
+            if (srcElement.classList.contains('flipper')) {
+                srcElement = srcElement.parentNode;
+            }
+            if (!srcElement.classList.contains('card')) {
                 return;
             }
 
-            var card = cards[srcElement.getAttribute('idx')];
+            var cardIndex = srcElement.getAttribute('idx');
+            var card = cards[cardIndex];
+
+            if (card.domElement !== srcElement) {
+                throw new Error('Incoherent card index: ' + cardIndex);
+            }
 
             if (card.freezed === 1 || openCards.contains(card)) {
                 return;
@@ -169,17 +106,33 @@ var Level = function (rows, cols, requiredMatchesCount, simultaneouslyRevealed) 
 
             if (openCards.length > 1) {
                 var lastOpenCard = openCards[openCards.length - 2];
-                if (lastOpenCard.pair === card.pair && openCards.length == requiredMatchesCount) { // Pair found !
+                if (lastOpenCard.pair === card.pair && openCards.length == lvlParams.requiredMatches) { // Pair found !
                     card.flip('faceUp');
-                    for (var i = 0; i < openCards.length; i = i + 1) {
+                    for (var i = 0; i < openCards.length; i++) {
                         openCards[i].freezed = 1;
                         openCards[i].pulse();
                     }
                     matchCount++;
                     openCards = [];
+                    if (lvlParams.panicMode) {
+                        if (lastPanicTranslation == 'vertical') {
+                            setTimeout(function (cardIndex) {
+                                var rowY = Math.floor(cardIndex / lvlParams.cols);
+                                triggerHorizontalTranslation(rowY);
+                            }, PULSE_DURATION, cardIndex);
+                            lastPanicTranslation = 'horizontal';
+                        } else {
+                            setTimeout(function (cardIndex) {
+                                var colX = cardIndex % lvlParams.cols;
+                                triggerVerticalTranslation(colX);
+                            }, PULSE_DURATION, cardIndex);
+                            lastPanicTranslation = 'vertical';
+                        }
+                    }
                 } else {
                     window.setTimeout(function () { // delayed after the face flip
-                        while (openCards.length > simultaneouslyRevealed && openCards.some(function (c) { return c.pair != card.pair})) {
+                        while (openCards.length > lvlParams.simultaneouslyRevealed
+                               && openCards.some(function (c) { return c.pair != card.pair})) {
                             var cardToHideIndex = openCards.findIndex(function (c) { return c.pair != card.pair});
                             openCards[cardToHideIndex].flip('faceDown');
                             openCards.splice(cardToHideIndex, 1); // remove card from array
@@ -192,36 +145,140 @@ var Level = function (rows, cols, requiredMatchesCount, simultaneouslyRevealed) 
                 card.flip('faceUp');
             }
 
-            if (matchCount === (rows * cols) / requiredMatchesCount) {
+            if (matchCount === (lvlParams.rows * lvlParams.cols) / lvlParams.requiredMatches) {
                 playfieldWrapper.className = 'win';
-
                 window.setTimeout(function () {
                     playfield.className = 'play-field win';
-                    self.onwin(clicksCnt, Math.round(((rows * cols) * 100) / clicksCnt));
+                    self.onwin(self.totalClicksCount, Math.round(((lvlParams.rows * lvlParams.cols) * 100) / self.totalClicksCount));
                     playfieldWrapper.className = '';
                 }, 1500);
 
             }
+        },
+        // Those 2 functions do 3 important things :
+        // - update the cards position inside $cards
+        // - update their idx attributes
+        // - update their style.left
+        triggerHorizontalTranslation = function (rowY) {
+            var rowYcards = cards.slice(rowY * lvlParams.cols, rowY * lvlParams.cols + lvlParams.cols);
+            rowYcards.forEach(function (card) {
+                var oldCardIndex = card.domElement.getAttribute('idx'),
+                    oldX = oldCardIndex % lvlParams.cols,
+                    newX = (oldX + 1) % lvlParams.cols,
+                    newCardIndex = rowY * lvlParams.cols + newX;
+                cards[newCardIndex] = card;
+                card.domElement.setAttribute('idx', newCardIndex);
+                card.domElement.style.left = (newX * 90) + 'px';
+            });
+        },
+        triggerVerticalTranslation = function (colX) {
+            var colXcards = cards.map(function (card, cardIndex) {
+                    if (cardIndex % lvlParams.cols == colX) {
+                        return card;
+                    }
+                }).filter(function (card) { return card; });
+            colXcards.forEach(function (card) {
+                var oldCardIndex = card.domElement.getAttribute('idx'),
+                    oldY = Math.floor(oldCardIndex / lvlParams.cols),
+                    newY = (oldY + 1) % lvlParams.rows,
+                    newCardIndex = newY * lvlParams.cols + colX;
+                cards[newCardIndex] = card;
+                card.domElement.setAttribute('idx', newCardIndex);
+                card.domElement.style.top = (newY * 90) + 'px';
+            });
         };
 
-    if ((rows * cols) / requiredMatchesCount > cardsList.length) {
-        throw ('There are not enough cards to display the playing field');
-    } else if ((rows * cols) % requiredMatchesCount !== 0) {
-        throw ('Out of bounds');
+    if (lvlParams.simultaneouslyRevealed >= lvlParams.requiredMatches) {
+        throw new Error('Incorrect level parameters');
+    } else if ((lvlParams.rows * lvlParams.cols) / lvlParams.requiredMatches > cardsList.length) {
+        throw new Error('There are not enough cards to display the playing field');
+    } else if ((lvlParams.rows * lvlParams.cols) % lvlParams.requiredMatches !== 0) {
+        throw new Error('$rows x $cols is not a multiple of $requiredMatches');
     }
 
-    playfieldWrapper.className = '';
     playfield.className = 'play-field';
+    playfield.style.left = '-' + (lvlParams.cols * 45) + 'px';
+    playfield.onmousedown = play;
 
-    cardsList = cardsList.split('');
     cardsList.shuffle();
 
-    this.onwin = function () {};
+    self.totalClicksCount = 0;
+    self.onwin = function () {};
 
     prepare();
     draw();
+};
 
-    playfield.onmousedown = play;
+var Card = function (text, pair, lvl) {
+    this.state      = 'faceDown';
+    this.freezed    = 0;
+    this.text       = text;
+    this.pair       = pair;
+    this.clicksCnt     = 0;
+
+    var flipper = null,
+        front   = null,
+        back    = null,
+        clicks  = null;
+
+    this.draw = function () {
+        var txt = document.createTextNode(this.text);
+
+        this.domElement = document.createElement('div');
+
+        flipper = this.domElement.cloneNode(false);
+
+        front   = this.domElement.cloneNode(false);
+        back    = this.domElement.cloneNode(false);
+        clicks  = this.domElement.cloneNode(false);
+
+        this.domElement.className = 'card';
+        flipper.className = 'flipper';
+        front.className = 'front face icon';
+        back.className = 'back face';
+        clicks.className = 'clicks';
+        clicks.appendChild(document.createTextNode('\xA0'));
+
+        front.appendChild(txt);
+        front.appendChild(clicks);
+
+        txt = txt.cloneNode(false);
+        txt.nodeValue = '\xA0';
+
+        back.appendChild(txt);
+
+        flipper.appendChild(back);
+        flipper.appendChild(front);
+
+        this.domElement.appendChild(flipper);
+    };
+
+    this.flip = function (state) {
+        if (state === 'faceUp') {
+            flipper.className = 'flipper flipfront';
+            front.style.opacity = 1;
+            back.style.opacity = 0;
+
+            this.state = 'faceUp';
+            this.clicksCnt++;
+
+            clicks.childNodes[0].nodeValue = this.clicksCnt;
+
+            lvl.totalClicksCount++;
+        } else if (state === 'faceDown') {
+            flipper.className = 'flipper flipback';
+            front.style.opacity = 0;
+            back.style.opacity = 1;
+            this.state = 'faceDown';
+        }
+    };
+
+    this.pulse = function () {
+        flipper.className = 'flipper flipfront pulse';
+        window.setTimeout(function () {
+            flipper.parentNode.style.opacity = '0.3';
+        }, PULSE_DURATION);
+    };
 };
 
 Array.prototype.shuffle = function () {
@@ -238,7 +295,7 @@ Array.prototype.shuffle = function () {
 Array.prototype.contains = function (value) {
     var i, result = false;
 
-    for (i = 0; i < this.length; i = i + 1) {
+    for (i = 0; i < this.length; i++) {
         if (this[i] === value) {
             result = true;
         }
